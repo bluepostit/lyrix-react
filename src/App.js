b15b7f913cdd517953a67b9ee5aee7f6b1575f3b
Yair Gordon <yairgordon@gmail.com>
2017-08-30 17:51:37 +0300

    added router, other stuff

--- src/App.js
+++ src/App.js
@@ -1,35 +1,136 @@
 import React, { Component } from 'react';
-import { Grid, Col, Row } from 'react-bootstrap';
-import logo from './logo.svg';
+import { Grid, Col, Row, ListGroup } from 'react-bootstrap';
+import { BrowserRouter as Router, Link, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-class Lyrics extends Component {
-	render() {
-		let lyrics = this.props.lyrics;
-		return (
-			<Grid className="lyricsContainer">
-				<Row className="show-grid">
-					<Col xs={12} md={8}>
-						<h1 className="title">{lyrics.title}</h1>
-					</Col>
-				</Row>
-				<Row className="show-grid">
-					<Col xs={12} md={8}><h2 className="artist">{lyrics.artist}</h2></Col>
-				</Row>
-				<Row className="show-grid">
-					<Col xs={12} md={8}><div className="lyrics">{lyrics.lyrics}</div></Col>
-				</Row>
-			</Grid>
-		);
-	}
+const Root = (props) => (
+    <div className="wrapper" {...props}/>
+);
+
+class LyricsListItem extends Component {
+  render() {
+    let item = this.props.item;
+    console.log('LyricsListItem::render()', item);
+    return (
+      <Link to={`/lyric/${item.id}`}>
+        <li id={item.id}
+            className="list-group-item lyrics-list-item"
+            key={item.id}>
+          <div className="visible-xs visible-md visible-lg">
+            <div className="title">{item.title}</div>
+            <div className="artist">{item.artist}</div>
+          </div>
+          <div className="visible-sm">
+            <span className="title">{item.title}</span>
+            <span className="dash">&mdash;</span>
+            <span className="artist">{item.artist}</span>
+          </div>
+        </li>
+      </Link>
+    );
+  }
 }
 
-class LyricsPage extends Component {
-	render() {
-		let simpleLyrics = {
-			title: "Wish You Were Here",
-			artist: "Pink Floyd",
-			lyrics: `So, so you think you can tell
+class LyricsList extends Component {
+  renderLyricsList() {
+    let lst = this.props.data;
+    return lst.map((item, i) => (
+      <LyricsListItem item={item} key={item.id} />
+    ));
+  }
+
+  render() {
+    return (
+      <ListGroup className="lyrics-list-container" componentClass="ul">
+        {this.renderLyricsList()}
+      </ListGroup>
+    );
+  }
+}
+
+class Lyrics extends Component {
+  render() {
+    let data = this.props.data;
+    return (
+      <Grid className="lyrics-container">
+        <Row className="show-grid">
+          <Col xs={12} md={8}>
+            <h1 className="title">{data.title}</h1>
+          </Col>
+        </Row>
+        <Row className="show-grid">
+          <Col xs={12} md={8}><h2 className="artist">{data.artist}</h2></Col>
+        </Row>
+        <Row className="show-grid">
+          <Col xs={12} md={8}><div className="lyrics">{data.lyrics}</div></Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+class MainPage extends Component {
+  render() {
+    let data = songData;
+    return (
+      <Router>
+        <Root>
+          <Grid className="visible-md visible-lg">
+            <Row className="show-grid">
+              <Col md={3}>
+                <div className="sidebar">
+                  <LyricsList data={data} />
+                </div>
+              </Col>
+              <Col md={9}>
+                <div className="mainPane">
+                  <Switch>
+                    <Route path="/lyric/:lyricId" render={({ match }) => (
+                      <Lyrics data={data.find(ly => ly.id == match.params.lyricId)} />
+                    )}/>
+                    <Route exact path="/" render={() => (
+                      <h1>No Song Selected</h1>
+                    )}/>
+                  </Switch>
+                </div>
+              </Col>
+            </Row>
+          </Grid>
+          <div className="visible-xs visible-sm">
+            <Switch>
+              <Route exact path="/">
+                <LyricsList className="visible-xs visible-sm" data={data} asSideBar={false}/>
+              </Route>
+              <Route path="/lyric/:lyricId" render={({ match }) => (
+                <Lyrics data={data.find(ly => ly.id == match.params.lyricId)} />
+              )}/>
+            </Switch>
+          </div>
+        </Root>
+      </Router>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+var songData = [
+  {
+    title: "All My Loving",
+    artist: "The Beatles",
+    id: '0',
+    lyrics: "Close your eyes while I kiss you..."
+  },
+  {
+    title: "Wish You Were Here",
+    artist: "Pink Floyd",
+    id: '1',
+    lyrics: `So, so you think you can tell
 Heaven from hell
 Blue skies from pain
 Can you tell a green field
@@ -53,11 +154,7 @@ Running over the same old ground
 And how we found
 The same old fears
 Wish you were here`
-		};
-		return (
-			<Lyrics lyrics={simpleLyrics} />
-		);
-	}
-}
+  }
+];
 
-export default LyricsPage;
+export default MainPage;
